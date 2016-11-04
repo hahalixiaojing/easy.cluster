588c66aa52dc8f914841ee7f2327245962069c45
lixiaojing <lee.bmw@foxmail.com>
2016-11-04 11:37:25 +0800

    提交文件

--- src/main/java/easy/cluster/directory/RedisSubscriberThread.java
+++ src/main/java/easy/cluster/directory/RedisSubscriberThread.java
@@ -4,22 +4,25 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public class RedisSubscriberThread {
+	private static final String CHANNEL = "redisRegistry";
+
 	private final JedisPool pool;
 	private Thread thread;
+	private volatile boolean isRunning = false;
 
 	public RedisSubscriberThread(JedisPool pool) {
 		this.pool = pool;
 	}
 
 	public void start() {
-
+		isRunning = true;
 		thread = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				while (true) {
+				while (isRunning) {
 					try (Jedis jedis = pool.getResource()) {
-						jedis.subscribe(new RedisSubscriber(), "redisRegistry");
+						jedis.subscribe(new RedisSubscriber(), CHANNEL);
 					} catch (Exception e) {
 					}
 				}
@@ -30,4 +33,7 @@ public class RedisSubscriberThread {
 		thread.start();
 	}
 
+	public void close() {
+		this.isRunning = false;
+	}
 }
